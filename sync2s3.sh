8880324e34b5714fb6059b1697b02b6d25da7bc8
ChangMM <1769111903@qq.com>
2019-01-07 22:02:53 +0800

    modify sync shell

--- sync2s3.sh
+++ sync2s3.sh
@@ -3,7 +3,7 @@
 npm run sub
 
 # sync local files to S3:bucket
-aws s3 sync ./dist s3://docs.pp.io/docs --acl public-read
+aws s3 sync .vuepress/dist s3://docs.pp.io/docs --acl public-read --exclude './git/*'
 
 # create cloudfront distribution invalidation
 aws cloudfront create-invalidation --distribution-id ECJMUYDIF91JL --paths /docs/*
