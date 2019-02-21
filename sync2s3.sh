4231f06c8f971cd6e499d0aec96ac1d684d258e5
ChangMM <1769111903@qq.com>
2019-02-22 00:44:11 +0800

    update sync2aws.sh

--- sync2s3.sh
+++ sync2s3.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# build
 npm run sub
 
+# delete all .DS_Store file
+find . -name '.DS_Store' -type f -delete
+
 # sync local files to S3:bucket
-aws s3 sync .vuepress/dist s3://docs.pp.io2/docs --acl public-read --exclude './git/*'
+aws s3 sync .vuepress/dist s3://docs.pp.io2/docs --acl public-read --exclude '.git/*'
 
 # create cloudfront distribution invalidation
 aws cloudfront create-invalidation --distribution-id E1VRFX5KBKXI4R --paths /docs/*
