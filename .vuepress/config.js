40a1042f166372b7a9ac900a1787006d28f57c31
ChangMM <1769111903@qq.com>
2019-02-28 16:02:00 +0800

    [dev] modify forum link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -51,7 +51,7 @@ module.exports = {
       { text: 'Docs', class: 'active', link: '/' },
       { text: 'Download', internal: true, link: 'https://www.pp.io/download.html' },
       { text: 'Blog', internal: true, link: 'https://blog.pp.io' },
-      { text: 'Forum', link: 'https://www.reddit.com/r/ppio' },
+      { text: 'Discrod', link: 'https://discord.gg/wAUED89' },
       { text: 'Github', link: 'https://github.com/ppio' },
     ],
     sidebar: {
